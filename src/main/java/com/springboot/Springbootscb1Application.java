d255ecf93e7bd0bfb923e4037442bd31be76774d
sumeet-s-g <149381260+sumeet-s-g@users.noreply.github.com>
2024-03-01 00:28:51 +0530

    updated code

--- src/main/java/com/springboot/Springbootscb1Application.java
+++ src/main/java/com/springboot/Springbootscb1Application.java
@@ -2,12 +2,19 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Springbootscb1Application {
+public class Springbootscb1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springbootscb1Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Springbootscb1Application.class);
 	}
 
 }
